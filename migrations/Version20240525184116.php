7c57d6420f611205733d3826b2a67eaa9ee0905d
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-05-25 20:42:07 +0200

    [+] mount docker env services

--- migrations/Version20240525184116.php
+++ migrations/Version20240525184116.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240525183437 extends AbstractMigration
+final class Version20240525184116 extends AbstractMigration
 {
     public function getDescription(): string
     {
