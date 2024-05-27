09951277291a66f947127c9eea6c9639afbe2051
Jakub Holas <jakubkoholas@gmail.com>
2024-05-27 20:03:02 +0200

    Add Twig support and configuration
    Integrate Twig into the project by updating the bundles.php, composer.json, and symfony.lock files to include the Symfony Twig bundle. Also, set up Twig testing configuration in a newly created twig.yaml file.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
