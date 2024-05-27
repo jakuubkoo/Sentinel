1e8c2b303bed7cfda313909e941a94b900c6f9ee
Jakub Holas <jakubkoholas@gmail.com>
2024-05-27 02:32:22 +0200

    Implement Symfony mailer functionality
    The commit adds Symfony mailer to the project, including updating environment variables, installing required packages, and creating a dedicated EmailManager class. This enables setting up and sending emails through the application. In addition, a new test route is added to the IndexController to send test emails.

---
+++ src/Manager/EmailManager.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Manager;
+
+use Exception;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class EmailManager
+{
+    /**
+     * @var MailerInterface $mailer
+     */
+    private MailerInterface $mailer;
+
+    /**
+     * Constructor method for the class.
+     *
+     * @param MailerInterface $mailer The mailer instance.
+     */
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Sends service down email to multiple recipients.
+     *
+     * @param array<string> $recipients The recipients of the email.
+     * @param string $serviceName The name of the service that is down.
+     *
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function sendServiceDownEmail(array $recipients, string $serviceName): void
+    {
+        // Sends multiple emails to all recipients
+        $this->sendMultipleEmails($recipients, $serviceName . ' Service is Down!', 'Service ' . $serviceName . ' is currently down. Please check your service.');
+    }
+
+    /**
+     * Sends multiple emails to an array of recipients.
+     *
+     * @param array<string> $recipients An array of email addresses of the recipients.
+     * @param string $title The title or subject of the email.
+     * @param string $body The email body content.
+     *
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function sendMultipleEmails(array $recipients, string $title, string $body): void
+    {
+        $email = (new Email())
+            ->from('support@edgetracker.app')
+            ->to(...$recipients)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject($title)
+            ->html('<p>' . $body . '</p>');
+
+        $this->mailer->send($email);
+    }
+}
